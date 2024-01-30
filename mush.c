5bf4e2c725fd36940026839a428c9ecd6f0355eb
TParr4196 <tparr@LAPTOP-0O0FGHKA.localdomain>
2024-01-29 20:16:01 -0800

    final commit

--- mush.c
+++ mush.c
@@ -4,17 +4,51 @@
 #include<unistd.h>
 #include<sys/wait.h>
 
+
+void commandLine(char* *commands){
+    execvp(commands[0],commands);
+    perror("command not registered");
+    exit(1);
+}
+
+
 int main(){
-    char buffer[2048];
     while(1==1){
-        printf("directory:\n");
+        char buffer[2048];
+        printf("mush: ");
         fgets(buffer, 2048, stdin);
+
         char* buf="";
-        buf=strtok(buffer," ");
+        buf=strtok(buffer," \n");
+        char* command[128];
+        
+        int i = 0;
         while(buf!=NULL){
-            puts(buf);
-            buf=strtok(NULL," ");
+            command[i]=buf;
+            buf=strtok(NULL," \n");
+            i++;
         }
-        printf("%s",buffer);
+        command[i]=NULL;
+        if(strcmp("cd",command[0])==0){
+            int check=chdir(command[1]);
+            if(check==-1){
+                perror("invalid directory");
+            }
+        }
+        else if(strcmp("exit",command[0])==0){
+            exit(0);
+        }
+        else if(command[0]!=NULL){
+            pid_t pid = fork();
+            if(pid<0){
+                perror("forking error");
+            }
+            else if(pid==0){
+                commandLine(command);
+            }
+            else {
+                wait(NULL);
+            }
+        }
     }
 }
